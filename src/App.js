bf6cf66a291d950d2a41ef58b5c34561511e54c9
R. Fariz <farizramazanov38@gmail.com>
2022-07-03 17:13:45 +0400

    feat: add state

--- src/App.js
+++ src/App.js
@@ -4,29 +4,42 @@ import Button from "./components/button/Button";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    btns: [
+      { term: "AC", bg: 'btn-bg', color: "gray"},
+      { term: "%", bg: 'btn-sm', color: "gray" },
+      { term: "/", bg: 'btn-sm', color: "orange" },
+      { term: "7", bg: 'btn-sm', color: "white" },
+      { term: "8", bg: 'btn-sm', color: "white"  },
+      { term: "9", bg: 'btn-sm', color: "white"  },
+      { term: "*", bg: 'btn-sm', color: "orange" },
+      { term: "4", bg: 'btn-sm', color: "white"  },
+      { term: "5", bg: 'btn-sm', color: "white"  },
+      { term: "6", bg: 'btn-sm', color: "white"  },
+      { term: "-", bg: 'btn-sm', color: "orange" },
+      { term: "1", bg: 'btn-sm', color: "white"  },
+      { term: "2", bg: 'btn-sm', color: "white"  },
+      { term: "3", bg: 'btn-sm', color: "white"  },
+      { term: "+", bg: 'btn-sm', color: "orange" },
+      { term: "0", bg: 'btn-bg', color: "white" },
+      { term: ".", bg: 'btn-sm', color: "white" },
+      { term: "=", bg: 'btn-sm', color: "orange" },
+    ],
+  };
+
   render() {
+    
+    const {btns} = this.state
+
     return (
       <div className="App">
         <Input />
         <div className="buttons">
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
+          {btns.map((btn, i) => {
+            return (
+              <Button key={i} color={btn.color} bg={btn.bg} term={btn.term}/>
+            )
+          })}
         </div>
       </div>
     );
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import "./Button.css"
 
 export default class Button extends Component {
+
   render() {
+
+    const {term, bg, color} = this.props
+
     return (
-      <button className='btn'>6</button>
+      <button className={`btn ${bg} ${color}`}>{term}</button>
     )
   }
 }
